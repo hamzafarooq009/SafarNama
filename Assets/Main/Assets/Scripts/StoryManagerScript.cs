5ddcafe4f2da81ed51d5b059fa9c2432956f2589
Ahmed Farhan <ahmedfarhan252@gmail.com>
2020-11-24 16:33:02 +0000

    Add: Multiple models and dialogues

--- Assets/Main/Assets/Scripts/StoryManagerScript.cs
+++ Assets/Main/Assets/Scripts/StoryManagerScript.cs
@@ -4,11 +4,13 @@ using Doublsb.Dialog;
 
 public class StoryManagerScript : MonoBehaviour
 {
+    // #TODO: PlayerPrefs
     public DialogManager DialogManager;
     public GameObject[] Image;
     private string exhibit_ch = "Exhibit";
     public bool dialogue_ongoing;
     public Dictionary<int, bool> storyData;
+    public Dictionary<int, string> exhibitList;
 
     // private void Awake()
     // {
@@ -16,11 +18,23 @@ public class StoryManagerScript : MonoBehaviour
     //     // Debug.Log(dialogue_ongoing);
     // }
 
+    //PlayerPrefs coins
+
     private void Start()
     {
         dialogue_ongoing = false;
         storyData = new Dictionary<int, bool>();
-        storyData.Add(0, false);
+        exhibitList = new Dictionary<int, string>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            storyData.Add(i, false);
+        }
+
+        exhibitList.Add(0, "Makhnu");
+        exhibitList.Add(1, "Queen Victoria");
+        exhibitList.Add(2, "<ENTER NAME HERE>");
+        exhibitList.Add(3, "<ENTER NAME HERE>");
     }
 
     private void Show_Image(int index)
@@ -30,8 +44,19 @@ public class StoryManagerScript : MonoBehaviour
 
     private void generateDialog(int index, List<DialogData> dialogTexts, int top, RectTransform rt, string ch, string question, string[] answers, int correct_choice_index, string correct_response, string wrong_response)
     {
+        if (index != 0)
+            Debug.Log("In Generate Dialog: " + storyData[index - 1]);
         if (index != 0 && !storyData[index - 1])
         {
+            //Will not let the user proceed until previous exhibit is finished.
+
+            // Get name of previous exhibit from the list of exhibits
+            // string exhibit_name = exhibit_list[index-1];
+
+            // Makhnu quote: "Visit the {model} exhibit before you can visit this one"
+            // string quote = "Visit the" +  {model} exhibit before you can visit this one"
+
+
             return;
         }
 
@@ -39,6 +64,10 @@ public class StoryManagerScript : MonoBehaviour
             return;
 
         dialogue_ongoing = true;
+
+
+        // var QuestionOnly = new DialogData(question, exhibit_ch);
+        // dialogTexts.Add(QuestionOnly);
 
         var Quiz = new DialogData(question, exhibit_ch);
         for (int i = 0; i < 4; i++)
@@ -55,15 +84,14 @@ public class StoryManagerScript : MonoBehaviour
 
         Quiz.Callback = () =>
         {
+            //TODO: Sleep
             rt.offsetMax = new Vector2(rt.offsetMax.x, -top); //revert top
 
             if (DialogManager.Result == "Correct")
             {
+                storyData[index] = true;
                 var answerDiag = new List<DialogData>();
-                answerDiag.Add(new DialogData(correct_response, exhibit_ch, () =>
-                {
-                    storyData[index] = true;
-                }));
+                answerDiag.Add(new DialogData(correct_response, exhibit_ch));
                 DialogManager.Show(answerDiag);
             }
             else if (DialogManager.Result == "Wrong")
@@ -101,10 +129,56 @@ public class StoryManagerScript : MonoBehaviour
         dialogTexts.Add(new DialogData("Text can be /speed:down/slow... /speed:init//speed:up/or fast.", ch, () => Show_Image(4)));
         dialogTexts.Add(new DialogData("You don't even need to click on the window like this.../speed:0.1/ tada!/close/", ch, () =>
         {
+            // TODO: Add timeout for last dialog
             rt.offsetMax = new Vector2(rt.offsetMax.x, top);
         }));
 
         generateDialog(0, dialogTexts, top, rt, ch, question, answers, correct_choice_index, correct_response, wrong_response);
+    }
+
+
+    public void QueenVictoria()
+    {
+        string ch = "Exhibit";
+        string question = "What is 2 times 5?";
+        string[] answers = { "10", "7", "5 * 2", "Why should I care?" };
+        int correct_choice_index = 0;
+        string correct_response = "You are right.";
+        string wrong_response = "You are wrong.";
+        int top = 130;
+        RectTransform rt = DialogManager.Printer.GetComponent<RectTransform>();
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("/size:up/Hi, /size:init/my name is Queen Victoria!", ch));
+        dialogTexts.Add(new DialogData("You don't even need to click on the window like this.../speed:0.1/ tada!/close/", ch, () =>
+        {
+            rt.offsetMax = new Vector2(rt.offsetMax.x, top);
+        }));
+
+        generateDialog(1, dialogTexts, top, rt, ch, question, answers, correct_choice_index, correct_response, wrong_response);
+    }
+
+
+    public void BahadurShahZafar()
+    {
+        string ch = "Exhibit";
+        string question = "What is 2 times 5?";
+        string[] answers = { "10", "7", "5 * 2", "Why should I care?" };
+        int correct_choice_index = 0;
+        string correct_response = "You are right.";
+        string wrong_response = "You are wrong.";
+        int top = 130;
+
+        RectTransform rt = DialogManager.Printer.GetComponent<RectTransform>();
+        var dialogTexts = new List<DialogData>();
+
+        dialogTexts.Add(new DialogData("/size:up/Hi, /size:init/my name is Bahadur Shah Zafar!", ch));
+        dialogTexts.Add(new DialogData("You don't even need to click on the window like this.../speed:0.1/ tada!/close/", ch, () =>
+        {
+            rt.offsetMax = new Vector2(rt.offsetMax.x, top);
+        }));
+
+        generateDialog(2, dialogTexts, top, rt, ch, question, answers, correct_choice_index, correct_response, wrong_response);
     }
 
 }
