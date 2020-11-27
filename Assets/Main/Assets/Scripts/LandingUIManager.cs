d292ed92ee013d493717918cba92cac21bc297be
Ahmed Farhan <ahmedfarhan252@gmail.com>
2020-11-27 22:01:21 +0000

    Cleared Assets and fixed reference error in LandingUIManager

--- Assets/Main/Assets/Scripts/LandingUIManager.cs
+++ Assets/Main/Assets/Scripts/LandingUIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SideMenuManager : MonoBehaviour
+public class LandingUIManager : MonoBehaviour
 {
     public Text coins;
     void OnEnable()
